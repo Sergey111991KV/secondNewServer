88d1806c112fcaa05bd6d53d690005508a5f6a91
Sergey111991KV <sergey.cosilov@gmail.com>
2020-09-01 17:35:43 +0300

    curl news

--- src/Domain/Service/Auth.hs
+++ src/Domain/Service/Auth.hs
@@ -5,7 +5,7 @@ import  Domain.ImportEntity
 import  ClassyPrelude
 
 
-class Monad m => Auth m where
+class (Monad m) => Auth m where
     findUsers                   :: Text -> Text -> m (Either Error User) -- password login
     newSession                  :: User -> m SessionId
     findUserBySession           :: SessionId -> m (Either Error User)
